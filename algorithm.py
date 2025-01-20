7109e40d79729ec67ba17e50cfd48b506a4df5fb
Mykhailo Artemiev <mykhailo.artemiev@icloud.com>
2025-01-20 14:40:13 +0000

    default external calls added

---
+++ algorithm.py
@@ -0,0 +1,16 @@
+class Algorithm:
+
+    def __init__(self):
+        pass
+
+    def FloorRequest(self, floor: int):
+        pass
+
+    def ElevatorRequest(self, floor: int, goUp: bool, elevator: int = 0):
+        pass
+
+    def ElevatorStep(self, floor: int, elevator: int = 0):
+        pass
+
+    def IsElevatorFull(self, isFull: bool, elevator: int = 0):
+        pass
