a40dab37fa9642376ff35576e1e1d582cb40c0f4
Mykhailo Artemiev <mykhailo.artemiev@icloud.com>
2025-01-26 22:23:18 +0000

    simulation updated to use new get and set methods

--- simulation.py
+++ simulation.py
@@ -10,15 +10,14 @@ if __name__ == "__main__":
 
     # temporary solution for testing purposes
     lift_manager = liftmanager.LiftManager(10, 3, 2, 0.5, 10)
-    lift_manager.target_floors[0][0] = 10
-    lift_manager.target_floors[1][0] = 15
-    lift_manager.target_floors[2][0] = 8
+    lift_manager.set_target_floors([5, 7, 2])
 
     # output lifts positions constantly
     while True:
         poss = lift_manager.lifts_positions()
         print(f'{"%.2f" % poss[0]} {"%.2f" % poss[1]} {"%.2f" % poss[2]}')
-        time.sleep(0.1)
+        print(f'{lift_manager.get_target_floors()}')
+        time.sleep(0.05)
 
 
         
