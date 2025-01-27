5de7a0ffa4812d167fc5f5d4e170c449279a0920
Mykhailo Artemiev <mykhailo.artemiev@icloud.com>
2025-01-27 14:37:46 +0000

    simulation changed to accomodate new test

--- simulation.py
+++ simulation.py
@@ -9,8 +9,8 @@ import liftmanager
 if __name__ == "__main__":
 
     # temporary solution for testing purposes
-    lift_manager = liftmanager.LiftManager(10, 3, 1, 0.1, 10)
-    lift_manager.set_target_floors([10, 15, 20])
+    lift_manager = liftmanager.LiftManager(10, 3, 2, 0.4, 10)
+    lift_manager.set_target_floors([10, 30, 20])
 
     # output lifts positions constantly
     while True:
@@ -20,6 +20,9 @@ if __name__ == "__main__":
         print(f'{"%.2f" % speed[0]} {"%.2f" % speed[1]} {"%.2f" % speed[2]}')
         print(*lift_manager.get_target_floors())
         time.sleep(0.05)
+
+        if lift_manager.lifts_positions()[1] > 20:
+            lift_manager.set_target_floors([10, 5, 20])
 
 
         
