cd6a4fc09468b4dad0f24a076baea5241268a3c5
Mykhailo Artemiev <mykhailo.artemiev@icloud.com>
2025-01-29 20:06:37 +0000

    added lift info to algorithms

--- algorithms.py
+++ algorithms.py
@@ -1,8 +1,9 @@
 from request import Request
+from lift import Lift
 
 class fcfs:
 
-    def run(self, list_of_requests: list[Request], picked_requests: list[Request]) -> Request:
+    def run(self,lift: Lift , list_of_requests: list[Request], picked_requests: list[Request]) -> Request:
         for picked_request in picked_requests:
             return picked_request
         for request in list_of_requests:
