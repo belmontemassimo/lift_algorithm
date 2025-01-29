c8696ec94e4b463029a331463150499df06df3c0
Mykhailo Artemiev <mykhailo.artemiev@icloud.com>
2025-01-29 20:05:34 +0000

    implemented fcfs

--- algorithms.py
+++ algorithms.py
@@ -1,8 +1,10 @@
 from request import Request
 
-
-
 class fcfs:
 
-    def run(self, list_of_requests: list[Request], picked_requests: list[Request]):
-        pass
+    def run(self, list_of_requests: list[Request], picked_requests: list[Request]) -> Request:
+        for picked_request in picked_requests:
+            return picked_request
+        for request in list_of_requests:
+            return request
+        return None
