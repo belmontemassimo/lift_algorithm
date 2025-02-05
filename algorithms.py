50e48443b95bdf1fe8206f63889c4986057dff40
momopj <70693419+momopj@users.noreply.github.com>
2025-02-05 16:45:29 +0000

    Added scan and changed fcfs

--- algorithms.py
+++ algorithms.py
@@ -3,11 +3,41 @@ from lift import Lift
 
 
 # create algorithm class for each algorithm
-class fcfs:
+class FCFS:
+    def run(self, lift: Lift, current_requests: list[Request], picked_requests: list[Request]) -> float:
+        if picked_requests:
+            return picked_requests[0].target_floor
+        
+        if current_requests:
+            return current_requests[0].request_floor
+        
+        return lift.current_floor
 
-    def run(self,lift: Lift , current_requests: list[Request], picked_requests: list[Request]) -> float:
-        for picked_request in picked_requests:
-            return picked_request.target_floor
-        for request in current_requests:
-            return request.request_floor
-        return None
+
+class SCAN:
+    def run(self, lift: Lift, current_requests: list[Request], picked_requests: list[Request]) -> float:
+        if not current_requests and not picked_requests:
+            return lift.current_floor  # No requests, stay on the same floor
+
+        # Get all floors from requests
+        all_requests = [req.target_floor for req in picked_requests] + [req.request_floor for req in current_requests]
+        all_requests = sorted(set(all_requests))  # Remove duplicates and sort
+
+        # Move in the current direction
+        if Request.direction == 1:
+            # Get next highest floor
+            for floor in all_requests:
+                if floor >= lift.current_floor:
+                    return floor
+            # If no more requests above, change direction
+            Request.direction = -1
+            return max(all_requests)  # Move to the highest request before reversing
+        
+        elif Request.direction == -1:
+            # Get next lowest floor
+            for floor in reversed(all_requests):
+                if floor <= lift.current_floor:
+                    return floor
+            # If no more requests below, change direction
+            lift.direction = 1
+            return min(all_requests)  # Move to the lowest request before reversing
