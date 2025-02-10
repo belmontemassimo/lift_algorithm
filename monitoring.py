92f88c9639eb942d670742ed26188af146026288
Mykhailo Artemiev <mykhailo.artemiev@icloud.com>
2025-02-10 06:24:17 +0000

    support for more then one lift

--- monitoring.py
+++ monitoring.py
@@ -34,20 +34,19 @@ class Monitoring:
         self.weight_label = StaticText(self.panel, -1, "", (25, 85))
         self.timer = StaticText(self.panel, -1, "", (25, 125))
         self.target_floor = StaticText(self.panel, -1, "", (25, 105))
-        self.speed_input = TextCtrl(self.panel, -1, "1", (220,55), (50,20))
-        self.speed_button = Button(self.panel, -1, "update", (285, 55), (70,20))
+        self.speed_input = TextCtrl(self.panel, -1, "1", (250,55), (50,20))
+        self.speed_button = Button(self.panel, -1, "update", (315, 55), (70,20))
         self.speed_button.Bind(EVT_BUTTON, self.on_speed_update)
-        self.algorithm_choice = Choice(self.panel, -1, (250,25), (100,20), self.algorithm.get_list())
+        self.algorithm_choice = Choice(self.panel, -1, (280,25), (100,20), self.algorithm.get_list())
         self.algorithm_choice.Bind(EVT_CHOICE, self.on_algorithm_choice)
         self.frame.Show()
 
     def update(self, timer:float):
-        state = self.lift_manager.get_states()[0]
-        self.position_label.SetLabelText(f'position:      {"%.2f" % self.lift_manager.get_positions()[0]}')
-        self.speed_label.SetLabelText(f'speed:         {"%.2f" % self.lift_manager.get_speed()[0]}')
-        self.state_label.SetLabelText(f'state:         {"waiting" if state == LiftState.WAITING else "idle" if state == LiftState.IDLE else "moving"}')
-        self.weight_label.SetLabelText(f'weight:        {"%.2f" % self.lift_manager.get_weight_kg()[0]}/{"%.2f" % self.lift_manager.capacity}')
-        self.target_floor.SetLabelText(f'target floor:  {"%.2f" % self.lift_manager.get_target_floors()[0]}')
+        self.position_label.SetLabelText(f'position:      {" ".join(["%.2f" % item for item in  self.lift_manager.get_positions()])}')
+        self.speed_label.SetLabelText(f'speed:         {" ".join(["%.2f" % item for item in self.lift_manager.get_speed()])}')
+        self.state_label.SetLabelText(f'state:         {" ".join(["waiting" if state == LiftState.WAITING else "idle" if state == LiftState.IDLE else "moving" for state in self.lift_manager.get_states()])}')
+        self.weight_label.SetLabelText(f'weight:        {" ".join(["%.2f" % item for item in  self.lift_manager.get_weight_kg()])} / {"%.2f" % self.lift_manager.capacity}')
+        self.target_floor.SetLabelText(f'target floor:  {" ".join(["%.2f" % item for item in self.lift_manager.get_target_floors()])}')
         self.timer.SetLabelText(f'time:  {"%.2f" % timer}')
         self.app.Yield()
 
