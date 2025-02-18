bc51b4d290b46443fd9f0c2713d3138fb788544c
momopj <70693419+momopj@users.noreply.github.com>
2025-02-18 21:03:20 +0000

    MYLIFT started

--- algorithms.py
+++ algorithms.py
@@ -3,6 +3,7 @@ from lift import Lift, LiftState
 from liftmanager import LiftManager
 from inspect import getmembers, isclass
 from importlib import import_module
+import math
 
 # get the name of the algorithms listed in the algorithms.py file and returns a dictionary conatining the name as key and the class as value
 def get_algorithms() -> dict[str,object]:
@@ -94,3 +95,137 @@ class LOOK:
             return down_requests[-1]
         
         return None
+    
+class MYLIFT:
+
+    max_batch_size = 5
+    min_batch_size = 1
+    scale_factor = 1
+    direction = Direction.UP
+    def __call__(self, lift: Lift, current_requests: list[Request], min_batch_size: int = 1, max_batch_size: int = 5, scale_factor: int = 1):
+        picked_requests = lift.picked_requests
+
+        if not current_requests and not picked_requests:
+            return None # No requests, lift remains idle
+
+        if lift.state == LiftState.IDLE:
+            return min(current_requests, key=lambda request: abs(request.request_floor - lift.position)).request_floor
+        
+        sorted_picked = sorted(picked_requests)
+        sorted_current = sorted(current_requests)
+
+        if not current_requests: #dealing with picked requests if there are no current requests in a LOOK manner
+            up_requests = [floor for floor in sorted_picked if floor > lift.position]
+            down_requests = [floor for floor in sorted_picked if floor < lift.position]
+
+            if up_requests and (self.direction == Direction.UP or not down_requests):
+                if self.direction != Direction.UP:
+                    self.direction = Direction.UP
+                return up_requests[0]
+        
+            if down_requests and (self.direction == Direction.DOWN or not up_requests):
+                if self.direction != Direction.DOWN:
+                    self.direction = Direction.DOWN
+                return down_requests[-1]
+        
+            return None
+        
+        if not picked_requests:#dealing with current requests if there are no current requests in a LOOK manner
+            up_requests = [floor for floor in sorted_current if floor > lift.position]
+            down_requests = [floor for floor in sorted_current if floor < lift.position]
+
+            if up_requests and (self.direction == Direction.UP or not down_requests):
+                if self.direction != Direction.UP:
+                    self.direction = Direction.UP
+                return up_requests[0]
+            
+            if down_requests and (self.direction == Direction.DOWN or not up_requests):        
+                if self.direction != Direction.DOWN:
+                    self.direction = Direction.DOWN
+                return down_requests[-1]
+            
+            return None
+        
+        #if the weight of the lift is above 80% of the capacity, the lift will ignore current requests and only deal with picked requests
+        if lift.weight > (0.8 * lift.capacity):
+            up_requests = [floor for floor in sorted_picked if floor > lift.position]
+            down_requests = [floor for floor in sorted_picked if floor < lift.position]
+
+            if up_requests and (self.direction == Direction.UP or not down_requests):
+                if self.direction != Direction.UP:
+                    self.direction = Direction.UP
+                return up_requests[0]
+        
+            if down_requests and (self.direction == Direction.DOWN or not up_requests):
+                if self.direction != Direction.DOWN:
+                    self.direction = Direction.DOWN
+                return down_requests[-1]
+        
+            return None
+        
+        if picked_requests and current_requests:
+
+            all_requests = set(sorted_picked + sorted_current)
+
+        
+            # Calculate batch size based on number of requests
+            batch_size = max(min_batch_size, min(max_batch_size, (len(all_requests)) // self.scale_factor))
+
+            # Split requests into batches of the calculated size
+            batches = [all_requests[i:i + batch_size] for i in range(0, len(all_requests), batch_size)]
+            
+            # If no batches, remain idle
+            if not batches:
+                return None
+            
+            # Process the batches
+            for i in range(len(batches)):
+                current_batch = batches[i]
+
+            # Separate requests into picked and current ones
+            pick_floors = [floor for floor in current_batch if floor in [req.target_floor for req in picked_requests]]
+            wait_floors = [floor for floor in current_batch if floor in [req.request_floor for req in current_requests]]
+
+            # Debug print to see the separation of requests
+            print(f"Picked Floors: {pick_floors}, Waiting Floors: {wait_floors}")
+
+            # If there are picked requests, prioritize them
+            if pick_floors:
+                if self.direction == Direction.UP:
+                    return pick_floors[0]  # Go to the next picked floor upwards
+                elif self.direction == Direction.DOWN:
+                    return pick_floors[-1]  # Go to the next picked floor downwards
+
+            # Serve waiting requests if they are in the direction of movement
+            if wait_floors:
+                if self.direction == Direction.UP:
+                    # Only serve current requests that are upwards
+                    up_requests = [floor for floor in wait_floors if floor > lift.position]
+                    if up_requests:
+                        return up_requests[0]  # Go to the next floor upwards
+                elif self.direction == Direction.DOWN:
+                    # Only serve current requests that are downwards
+                    down_requests = [floor for floor in wait_floors if floor < lift.position]
+                    if down_requests:
+                        return down_requests[-1]  # Go to the next floor downwards
+
+            return None
+
+
+
+
+        
+
+        
+
+        
+
+
+        
+
+
+
+
+        
+        
+
