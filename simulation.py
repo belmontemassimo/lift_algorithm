d2be2dd73ac97415c1d59efa84c79aeab136ab04
NoeBouchard <noyouchka.bouchard@gmail.com>
2025-02-18 18:52:03 +0000

    started to implement features

--- simulation.py
+++ simulation.py
@@ -4,19 +4,15 @@
 
 from gui import run_gui, gui_update
 from lift import LiftState
-from extenders import DeltaTime, set_time_multiplier
+from extenders import DeltaTime, set_time_multiplier, get_number_of_floors, get_number_of_lifts, start_simulation
 from request import Request
 from algorithms import AlgorithmHandler
 from monitoring import Monitoring
 from liftmanager import LiftManager
 from graph import SimulationAnalytics
 
-if __name__ == "__main__":
 
-    isMonitoring: bool = True
-    isGUI: bool = True
-    num_floors: int = 30
-    num_lifts: int = 3 # PLEASE DO NOT SET MORE THEN 3, NOT ALL MODULES ARE READY FOR IT :)
+def run_simulation(num_floors: int = 30, num_lifts: int = 3, isMonitoring: bool = True, isGUI: bool = True):
     max_speed: float = 2
     acceleration: float = 0.4
     capacity: float = 1000
@@ -24,25 +20,27 @@ if __name__ == "__main__":
 
     # requests are in the form of (target_floor, direction, time_created)
     list_of_requests: list[Request] = [Request(5,0,0), Request(8, 2, 3), Request(2, 1, 20), Request(3, 20, 21), Request(4, 1, 24), Request(5, 1, 25), 
-                                       Request(6, 1, 26), Request(7, 1, 27), Request(8, 1, 28), Request(9, 1, 29), Request(10, 1, 30), Request(11, 1, 31), 
-                                       Request(12, 1, 32), Request(13, 1, 33), Request(14, 1, 34), Request(15, 1, 35), Request(16, 1, 36), Request(17, 1, 37), 
-                                       Request(18, 1, 38), Request(19, 1, 39), Request(20, 1, 40), Request(21, 1, 41), Request(22, 1, 42), Request(23, 1, 43)]
+                                    Request(6, 1, 26), Request(7, 1, 27), Request(8, 1, 28), Request(9, 1, 29), Request(10, 1, 30), Request(11, 1, 31), 
+                                    Request(12, 1, 32), Request(13, 1, 33), Request(14, 1, 34), Request(15, 1, 35), Request(16, 1, 36), Request(17, 1, 37), 
+                                    Request(18, 1, 38), Request(19, 1, 39), Request(20, 1, 40), Request(21, 1, 41), Request(22, 1, 42), Request(23, 1, 43)]
     len_list_of_requests: int = len(list_of_requests)
     current_requests: list[Request] = []
     completed_requests_list: list[Request] = []
     algorithm = AlgorithmHandler()
     
-
-
-    lift_manager = LiftManager(num_floors, num_lifts, max_speed, acceleration, capacity, waiting_time)
+    lift_manager = LiftManager(get_number_of_floors(), get_number_of_lifts(), max_speed, acceleration, capacity, waiting_time)
     analytics = SimulationAnalytics()
+    
     if isMonitoring:
         monitoring = Monitoring(lift_manager, algorithm)
     else:
         algorithm.set_algorithm("FCFS")
         set_time_multiplier(1)
+
+        
+    gui_position_queue = None
     if isGUI:
-        gui_possition_queue = run_gui(num_floors, num_lifts)
+        gui_position_queue = run_gui(get_number_of_floors(), get_number_of_lifts())
 
     # TEMP
     update_flag: bool = False
@@ -50,6 +48,17 @@ if __name__ == "__main__":
     # set a timer so that we can see the efficiency of the algorithm based on a set of requests
     timer = 0
     deltatime = DeltaTime()
+    
+    # Wait for start signal from monitoring
+    while not start_simulation():
+        if isMonitoring:
+            monitoring.update(timer)
+        continue
+        
+    # Initialize GUI only after start signal
+    if isGUI:
+        gui_position_queue = run_gui(get_number_of_floors(), get_number_of_lifts())
+    
     while True: 
         timer += deltatime()
         # update lifts
@@ -62,8 +71,8 @@ if __name__ == "__main__":
         if isMonitoring:
             monitoring.update(timer)
         if isGUI:
-            gui_update(lift_manager, gui_possition_queue)
-        # check if there are any requests that should be processed
+            gui_update(lift_manager, gui_position_queue)
+
         new_requests_list = [request for request in list_of_requests if timer >= request.time_created]
         # if there is a request in new_requests_list then add it to the current_requests list 
         if new_requests_list:
@@ -72,7 +81,6 @@ if __name__ == "__main__":
             list_of_requests = [request for request in list_of_requests if request not in new_requests_list]
             update_flag = True
 
-       
         for lift in lift_manager.lifts:
             match lift.state:
                 case LiftState.WAITING:
@@ -96,4 +104,9 @@ if __name__ == "__main__":
 
             if len_list_of_requests == len(completed_requests_list):
                 break
+                
     analytics.generate_graphs(completed_requests_list, timer)
+    return analytics
+
+if __name__ == "__main__":
+    run_simulation()
