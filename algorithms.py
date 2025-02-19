18d9e26d0af1da6c100ec6f9d94b93015a38d0f6
momopj <70693419+momopj@users.noreply.github.com>
2025-02-19 20:08:55 +0000

    dskj

--- algorithms.py
+++ algorithms.py
@@ -187,8 +187,8 @@ class MYLIFT:
                 current_batch = batches[i]
 
                 # Separate requests into picked and current ones
-                pick_floors = [request for request in current_batch if request in [req.target_floor for req in picked_requests]]
-                wait_floors = [request for request in current_batch if request in [req.request_floor for req in current_requests]]
+                pick_floors = [request for request in current_batch if request.target_floor in [req.target_floor for req in picked_requests]]
+                wait_floors = [request for request in current_batch if request.request_floor in [req.request_floor for req in current_requests]]
 
                 # Debug print to see the separation of requests
                 print(f"Picked Floors: {pick_floors}, Waiting Floors: {wait_floors}")
