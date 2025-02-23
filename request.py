d738b5bb5ac81fe8a8c322da24cc9658d25c7964
Mykhailo Artemiev <mykhailo.artemiev@icloud.com>
2025-02-23 22:08:39 +0000

    commented request.py

--- request.py
+++ request.py
@@ -1,11 +1,14 @@
 from enum import Enum
 from extenders import double_normal_distribution
  
+# direction of a requets 
 class Direction(Enum):
     UP = 1
     DOWN = -1
 
+# class that represcent entier request from start to finish
 class Request:
+    # variables definition
     request_floor: int
     target_floor: int
     time_created: float    
@@ -15,14 +18,18 @@ class Request:
     direction: Direction
     lift_position_on_request: float | None  # Position of the lift when request was received
 
+    # init request 
     def __init__(self, request_floor:int , target_floor: int, time_created: float, weight_captor: int = 0):
         self.request_floor = request_floor
         self.target_floor = target_floor
         self.time_created = time_created
+        # determine direction of request (button press simulation)
         self.direction = Direction.UP if (request_floor-target_floor) < 0 else Direction.DOWN
         self.lift_position_on_request = None  # Initialize as None
+        # set weight to custom if provided
         if weight_captor != 0:
             self.weight_captor = weight_captor * 100
+        # generate random weight 
         else:
             self.weight_captor = int(double_normal_distribution(mean=70, std_dev=12.5, second_mean=3.5, second_std_dev=0.5)*100)
 
@@ -30,10 +37,12 @@ class Request:
     def waiting_time(self, current_time: float) -> float:
         return current_time - self.time_created
     
+    # saves time from request appearing to lift arrival
     def lift_check_in(self, time: float):
         self.time_on_floor = time - self.time_created
         return True
     
+    # saves time from lift arrival to request completion
     def floor_check_in(self, time: float):
         self.time_in_lift = time - self.time_on_floor
         return True
