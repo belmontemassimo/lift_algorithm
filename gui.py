b8a0b9f3192d1feb5abc9db9131bdfc38e5cc5c1
belmontemassimo <belmontemassimo@icloud.com>
2025-03-01 17:36:43 +0100

    commented gui

--- gui.py
+++ gui.py
@@ -6,6 +6,8 @@ import tkinter as tk
 from multiprocessing import Process, Queue
 from liftmanager import LiftManager
 
+
+# Lift colors
 COLORS = [
     "#1C2526",  # Dark Slate Gray
     "#3C2F2F",  # Dark Brown
@@ -20,6 +22,8 @@ COLORS = [
     "#1F2F3C",  # Dark Steel Blue
     "#3A2A29"   # Dark Mahogany
 ]
+
+# Lift shadows' colors
 TRANSPARENT_COLORS = [
     "#788081",  # Lightened Slate Gray
     "#8D7F7F",  # Lightened Brown
@@ -36,14 +40,9 @@ TRANSPARENT_COLORS = [
 ]
 
 
-# nothing is implemented yet
 class GUI:
 
-    queue: Queue
-    root: tk.Tk
-
     def __init__(self, n_floors: int, n_lifts: int, gui_queue):
-
         
         self.gui_queue:Queue = gui_queue
 
@@ -57,16 +56,16 @@ class GUI:
         self.seers = list()
         self.root.update()
         self.shadow_lifts = list()
-
-        proportional_stops = self.canvas.winfo_height()/n_floors
+        proportional_stops = self.canvas.winfo_height()/n_floors  # Distance between floors
         height = self.canvas.winfo_height()
+
         # Apply dimensions
         for i in range(n_floors+1):
             bottom_coord = height - (i * proportional_stops)
             self.canvas.create_line(0, bottom_coord, self.canvas.winfo_width(), bottom_coord, fill="black", width=2) #add line
             self.seers.append({'floor':i,'id':self.canvas.create_text(10, bottom_coord-(proportional_stops/2), text=str(i), fill="black", font=("Arial", 12, "bold")),'status':False}) #add floor label
         
-        # Create lift objects
+        # Create lift objects and their shadows
         for i in range(1, (2*n_lifts) + 1):
             if i%2 == 1:
                 lift_color = COLORS[int(((i-1)/2)%len(COLORS))]
@@ -78,22 +77,27 @@ class GUI:
         self.root.after(1,self.queue_manager)
         self.root.mainloop()
     
+
     def queue_manager(self):
+        # Get data from queue and call functions shadow movement, move and seers_update accordingly
+
         if not self.gui_queue.empty():  # Ensure there is data
             queue = self.gui_queue.get()
-            next_positions = queue['positions']
-            current_targets = queue['targets']
+            next_positions = queue['positions']  # Current lift positions
+            current_targets = queue['targets']   # Target floors for lifts
 
-            self.shadow_movements(current_targets)
-            self.move(next_positions)
-            self.seers_update(current_targets)
+            self.shadow_movements(current_targets) # Move shadow to target floor
+            self.move(next_positions) # Move lift to current position
+            self.seers_update(current_targets) # Change seers colors
 
         self.root.after(1, self.queue_manager)  # Avoid recursion crash
 
 
     def shadow_movements(self, current_targets):
+        # Move shadow lifts to preview target floors or align with idle lifts
         for i, target in enumerate(current_targets):
-            if target:
+            
+            if target:  # If lift has a target floor
                 self.canvas.update()
                 front_coords = self.canvas.coords(self.shadow_lifts[i])  # Get lift rectangle coordinates
                 current_y = front_coords[3]  # Use the bottom coordinate x1,y1,x2,y2)
@@ -108,7 +112,7 @@ class GUI:
                 elif difference < 0:  # Move UP
                     self.canvas.move(self.shadow_lifts[i], 0, -step)
             
-            else:
+            else: # If no target floor, lift shadow goes same coordinates as corresponding lift
                 lift_coords = self.canvas.coords(self.lifts[i])[3]
                 shadow_lift_coords = self.canvas.coords(self.shadow_lifts[i])[3]
                 if lift_coords != shadow_lift_coords:
@@ -118,6 +122,7 @@ class GUI:
 
 
     def move(self, positions):
+        # Move lifts to their current positions from LiftManager
         for i in range(len(self.lifts)):
             back_coords = positions[i]  # Target Y position
             self.canvas.update()
@@ -136,6 +141,7 @@ class GUI:
                 self.canvas.move(self.lifts[i], 0, -step)
 
     def seers_update(self,current_targets):
+        # Update floor labels (seers) to reflect target activity
         for seer in self.seers:
 
             if seer['floor'] in current_targets and seer['status'] == False:
@@ -150,11 +156,13 @@ class GUI:
                 continue
 
 def run_gui(num_floors: int, num_lifts: int) -> Queue:
+    # Start GUI in a separate process and return queue for communication
     gui_queue: Queue = Queue()
 
     Process(target=GUI, args=(num_floors, num_lifts, gui_queue)).start()
     return gui_queue
 
 def gui_update(lift_manager: LiftManager, gui_queue: Queue):
+    # Send lift positions and targets to GUI via queue
     if gui_queue.empty():
         gui_queue.put({'positions':lift_manager.get_positions(), 'targets':lift_manager.get_target_floors()})
